53ca5f97fc4267e86e32e7c1c92da4a75ea5c9ce
  dimabielov <dimabielov@Noutbuk-dimabielov.local>
2024-09-30 18:11:30 +0300

    fix mistake in name

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 
 const nextConfig = {
-    basePath: isProd ? '/Uspace' : '',  // Замініть 'your-repo-name' на назву вашого репозиторію
-    assetPrefix: isProd ? '/Uspace/' : '',
+    basePath: isProd ? '/Uspacy' : '',  // Замініть 'your-repo-name' на назву вашого репозиторію
+    assetPrefix: isProd ? '/Uspacy/' : '',
     output: 'export',
     distDir: 'dist',
 };
